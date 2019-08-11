33b00fa10b639823c7238c2d38627ccb150b1338
matatabinoneko <matatabinoneko0721@gmail.com>
2019-08-11 22:34:10 +0900

    solve ABC 137 and so on

--- ABC/137/a.cpp
+++ ABC/137/a.cpp
@@ -3,4 +3,7 @@ using namespace std;
 #define ll long long
 
 int main(void){
-}
+    int a,b; cin >> a >> b;
+    cout << max(a+b,max(a-b,a*b)) << endl;
+    return 0;
+}   
--- ABC/137/c.cpp
+++ ABC/137/c.cpp
@@ -2,5 +2,31 @@
 using namespace std;
 #define ll long long
 
-int main(void){
+int main(void){ 
+    int n; cin >> n;
+    vector<string>s(n);
+    for(int i=0;i<n;i++){
+        cin >> s[i];
+        // cout << s[i] << endl;
+        sort(s[i].begin(),s[i].end());
+        // cout << s[i] << endl;
+    }
+    sort(s.begin(),s.end());
+
+    // for(int i=0;i<n;i++){
+    //     cout << s[i] << endl;
+    // }
+
+    ll cnt=1,ans=0;
+    for(int i=0;i<=n-1;i++){
+        if(i!= n-1 && s[i]==s[i+1]){
+            cnt++;
+        } 
+        else{
+            ans += cnt*max(1,int(cnt-1))/2;
+            cnt=1;
+        } 
+    }
+    cout << ans << endl;
+    return 0;
 }
