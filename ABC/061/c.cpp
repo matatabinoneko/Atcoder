95bc1955a64d72b3278a9641c5c6d51e43f409ea
matatabinoneko <matatabinoneko0721@gmail.com>
2019-12-13 21:37:22 +0900

    solve ABC 061 C

--- ABC/061/c.cpp
+++ ABC/061/c.cpp
@@ -1,22 +1,46 @@
+// #include<bits/stdc++.h>
+// using namespace std;
+// #define ll long long
+// #define max_a 1000000
+
+// vector<ll> cnt(max_a,0);
+
+// int main(void){
+//     int n,k; cin >> n >> k;
+//     for(int i=0;i<n;i++){
+//         ll a,b; cin >> a >> b;
+//         cnt[a] += b;
+//     }
+//     for(int i=1;i<=max_a;i++){
+//         if(0<k) k -= cnt[i];
+//         if(k<=0){
+//             cout << i << endl;
+//             break;
+//         }
+//     }
+//     return 0;
+// }
+
 #include<bits/stdc++.h>
 using namespace std;
-#define ll long long
-#define max_a 1000000
+#define int long long
+typedef pair<int,int >P;
 
-vector<ll> cnt(max_a,0);
-
-int main(void){
+signed main(void){
     int n,k; cin >> n >> k;
+    vector<P>p;
     for(int i=0;i<n;i++){
-        ll a,b; cin >> a >> b;
-        cnt[a] += b;
+        int a,b; cin >> a >> b;
+        p.push_back(P{a,b});
     }
-    for(int i=1;i<=max_a;i++){
-        if(0<k) k -= cnt[i];
+    sort(p.begin(),p.end());
+    for(int i=0;i<p.size();i++){
+        k -= p[i].second;
         if(k<=0){
-            cout << i << endl;
+            cout << p[i].first << endl;
             break;
-        }
+        } 
     }
     return 0;
 }
+
